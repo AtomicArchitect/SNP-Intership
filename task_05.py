35abc56742af652ac17dedc56b14068e92184fab
Kirill Morozov <mokinotavailable@gmail.com>
2025-04-29 12:51:48 +0000

    05 Task Solved

---
+++ task_05.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+def date_in_future(increment):
+    return ((datetime.now()
+             if increment is None or type(increment) != int or increment < 0
+             else datetime.now() + timedelta(days=increment))
+            .strftime("%Y-%m-%d %H:%M:%S"))
+
+print(date_in_future([]))
+print(date_in_future(2))
+print(date_in_future(-10))
+print(date_in_future(31))
+print(date_in_future(1))
+print(date_in_future(0))
