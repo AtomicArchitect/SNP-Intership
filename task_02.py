5249732ea3778e71f229294944b704277be43918
Kirill Morozov <mokinotavailable@gmail.com>
2025-04-29 12:39:11 +0000

    02 Task Add internal list check

--- task_02.py
+++ task_02.py
@@ -5,8 +5,10 @@ def coincidence(source_list = None, rng = range(0, 0)):
             if item is None: continue
             if type(item) == int and item in rng: result.append(item)
             if type(item) == float and round(item) in rng: result.append(item)
+            if type(item) == list: result.extend(coincidence(item, rng))
     return result
 
 assert (coincidence([1, 2, 3, 4, 5], range(3, 6)) == [3, 4, 5])
 assert (coincidence() == [])
 assert (coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4)) == [1, 2, 2.5])
+assert (coincidence([1, 4, 5, [2.5, 25, 2, 3]], range(3, 6)) == [4, 5, 3])
