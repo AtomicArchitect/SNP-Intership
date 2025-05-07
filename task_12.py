86944c1709f676e08b88f6e6e8eeb0b79780caed
Kirill Morozov <mokinotavailable@gmail.com>
2025-05-07 18:44:53 +0000

    Task 12 Made variables non-private

--- task_12.py
+++ task_12.py
@@ -3,21 +3,21 @@ from task_11 import Dessert
 class JellyBean(Dessert):
 
     def is_delicious(self):
-        if self.__flavor is not None and type(self.__flavor) is str and self.__flavor == "black licorice": return False
+        if self.flavor is not None and type(self.flavor) is str and self.flavor == "black licorice": return False
         return super().is_delicious()
 
     def __init__(self, name = "JellyBean", calories = 0, flavor = None):
         super().__init__(name, calories)
-        self.__flavor = flavor
+        self.flavor = flavor
 
     def set_flavor(self, flavor):
-        self.__flavor = flavor
+        self.flavor = flavor
 
     def get_flavor(self):
-        return self.__flavor
+        return self.flavor
 
     def __str__(self):
-        return "Dessert: {}, Calories: {}, Flavor: {}".format(self.get_name(), self.get_calories(), self.__flavor)
+        return "Dessert: {}, Calories: {}, Flavor: {}".format(self.get_name(), self.get_calories(), self.flavor)
 
 jellyBean = JellyBean(flavor="black licorice")
 assert jellyBean.is_delicious() == False
